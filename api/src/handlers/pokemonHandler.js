d78ef17da4512df534546973c00a60c2734174c2
Augusto-AI <developer@nestech.us>
2023-07-22 12:39:33 -0500

    MVP
    First Commit

---
+++ api/src/handlers/pokemonHandler.js
@@ -0,0 +1,68 @@
+const {
+  getAllpokemon,
+  postPokemons,
+  getPokemonId,
+} = require("../controllers/pokemonControllers");
+
+//*----------------------------
+
+//* GET | /pokemons
+//*Obtiene un arreglo de objetos, donde cada objeto es un pokemon con su información.
+//* ==>
+
+const getPokemonHandler = async (req, res) => {
+  const { name } = req.query;
+  try {
+    if (name) {
+      const response = await getAllpokemon(name);
+      return res.status(200).json(response);
+    }
+    const response = await getAllpokemon();
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//*----------------------------------------------------------
+
+//* GET | /pokemons/:idPokemon
+//*Esta ruta obtiene el detalle de un pokemon específico. Es decir que devuelve un objeto con la información pedida en el detalle de un pokemon.
+//*El pokemon es recibido por parámetro (ID).
+//*Tiene que incluir los datos del tipo de pokemon al que está asociado.
+//*Debe funcionar tanto para los pokemones de la API como para los de la base de datos.
+//* ==>
+
+const getPokemonHandlerId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await getPokemonId(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//*-----------------------------------------------------------------
+
+//*POST | /pokemons
+//*Esta ruta recibirá todos los datos necesarios para crear un pokemon y relacionarlo con sus tipos solicitados.
+//*Toda la información debe ser recibida por body.
+//*Debe crear un pokemon en la base de datos, y este debe estar relacionado con sus tipos indicados (debe poder relacionarse al menos con dos).
+//* ==>
+
+const postPokemonHandler = async (req, res) => {
+  try {
+    const response = await postPokemons(req.body);
+    console.log(response);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getPokemonHandler,
+  postPokemonHandler,
+  getPokemonHandlerId,
+};
---
+++ client/src/Components/NavBar/navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { getNamePokemon, getPokemon } from "../../Redux/actions";
+import style from "./navBar.module.css";
+import Pokemon from "../../Imagenes/Pokemon.png";
+
+const NavBar = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  const handleSearchInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSearchButtonClick = () => {
+    dispatch(getNamePokemon(name));
+  };
+
+  function handleClick(e) {
+    e.preventDefault();
+    dispatch(getPokemon());
+  }
+
+  return (
+    <div className={style.navbar}>
+      <div>
+        <p className={style.pnav}></p>
+      </div>
+      <div>
+        <img src={Pokemon} alt="Pokemon Logo" className={style.logo} />
+      </div>
+
+      <div className={style.containernav}>
+        <ul>
+          <li className={style.linknavli}>
+            <Link to="/Create">CREATE A NEW POKEMON</Link>
+          </li>
+        </ul>
+        <div>
+          <input
+            className={style.navinput}
+            type="text"
+            value={name}
+            onChange={handleSearchInputChange}
+            placeholder="Find a Pokemon..."
+          />
+          <button className={style.buttonnav} onClick={handleSearchButtonClick}>
+            <span>FIND</span>
+          </button>
+        </div>
+        <ul>
+          <li className={style.linknavli2}>
+            <Link
+              to="/home"
+              onClick={(e) => {
+                handleClick(e);
+              }}
+            >
+              HOME
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
